7d0ec193bee19567075e11946a8ee724eae85dc2
hendisantika <hendisantika@yahoo.co.id>
2024-01-02 05:07:20 +0700

    Add getAllTags in TagController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/multitenancy/controller/TagController.java
+++ src/main/java/com/hendisantika/multitenancy/controller/TagController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.multitenancy.controller;
 
+import com.hendisantika.multitenancy.entity.Tag;
 import com.hendisantika.multitenancy.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TagController {
 
-    private final TagService service;
+    private final TagService tagService;
+
+    @GetMapping("/v1/tags")
+    public ResponseEntity<List<Tag>> getAllTags() {
+        List<Tag> list = tagService.getAllTags();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
