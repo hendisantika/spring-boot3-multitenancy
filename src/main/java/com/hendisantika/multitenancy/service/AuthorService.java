dc5d72edd1783171899b776d13ef11fbf7b75682
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:42:18 +0700

    Add authorRepository in AuthorService 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/multitenancy/service/AuthorService.java
+++ src/main/java/com/hendisantika/multitenancy/service/AuthorService.java
@@ -2,10 +2,12 @@ package com.hendisantika.multitenancy.service;
 
 import com.hendisantika.multitenancy.SpringBoot3MultitenancyApplication;
 import com.hendisantika.multitenancy.entity.Author;
+import com.hendisantika.multitenancy.exception.DataNotFoundException;
 import com.hendisantika.multitenancy.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 /**
@@ -29,5 +31,14 @@ public class AuthorService {
 
         List<Author> authorList = authorRepository.findAll();
         return authorList;
+    }
+
+    public Author getById(Long id) {
+        return authorRepository
+                .findById(id)
+                .orElseThrow(
+                        () ->
+                                new DataNotFoundException(
+                                        MessageFormat.format("Author id {0} not found", String.valueOf(id))));
     }
 }
