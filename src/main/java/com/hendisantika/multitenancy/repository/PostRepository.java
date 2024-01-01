d9fba29d1744f3f74a5e10da9dbe87e146829cb5
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:27:49 +0700

    Add selectPosts in PostRepository 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/multitenancy/repository/PostRepository.java
+++ src/main/java/com/hendisantika/multitenancy/repository/PostRepository.java
@@ -4,7 +4,11 @@ import com.hendisantika.multitenancy.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,5 +23,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+    @Modifying
+    @Query(value = "SELECT p FROM Post p WHERE lower(p.title) LIKE lower(concat('%', ?1,'%'))")
+    List<Post> selectPosts(String title);
 
 }
