2657bdbee9421c1e4925f3aec43fca266c358706
hendisantika <hendisantika@yahoo.co.id>
2024-01-02 05:02:06 +0700

    Add getAllAuthors in AuthorController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/multitenancy/controller/AuthorController.java
+++ src/main/java/com/hendisantika/multitenancy/controller/AuthorController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.multitenancy.controller;
 
+import com.hendisantika.multitenancy.entity.Author;
 import com.hendisantika.multitenancy.service.AuthorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +27,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthorController {
 
-    private final AuthorService service;
+    private final AuthorService authorService;
+
+    @GetMapping("/v1/authors")
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        List<Author> list = authorService.getAllAuthors();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
