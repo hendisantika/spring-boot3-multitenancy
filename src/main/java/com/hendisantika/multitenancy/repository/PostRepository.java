f5873ff15a314f1a81207177dbbaa28817f51f80
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:26:51 +0700

    Add findByTitleContainingIgnoreCase in PostRepository 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/multitenancy/repository/PostRepository.java
+++ src/main/java/com/hendisantika/multitenancy/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.hendisantika.multitenancy.repository;
 
 import com.hendisantika.multitenancy.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +18,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+
 }
