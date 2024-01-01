9f0e8a8ebcdc46d14d70a9c1405c1d5d857ea918
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:15:25 +0700

    Create starter project 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/multitenancy/SpringBoot3MultitenancyApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.multitenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3MultitenancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3MultitenancyApplication.class, args);
+    }
+
+}
