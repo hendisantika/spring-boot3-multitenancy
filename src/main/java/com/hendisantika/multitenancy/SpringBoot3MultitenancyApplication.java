1c17a7bd6db4920f15fa9a6c0429b56698c71b07
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:41:03 +0700

    Modify Main App 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/multitenancy/SpringBoot3MultitenancyApplication.java
+++ src/main/java/com/hendisantika/multitenancy/SpringBoot3MultitenancyApplication.java
@@ -2,9 +2,13 @@ package com.hendisantika.multitenancy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
-@SpringBootApplication
+import java.util.Properties;
+
+@SpringBootApplication(exclude = {FlywayAutoConfiguration.class})
 public class SpringBoot3MultitenancyApplication {
+    public static final Properties defaultProperties = new Properties();
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot3MultitenancyApplication.class, args);
