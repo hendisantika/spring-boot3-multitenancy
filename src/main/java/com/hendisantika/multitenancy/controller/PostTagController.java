ec461dec726f1f1dbb1b475195c12c243590b68e
hendisantika <hendisantika@yahoo.co.id>
2024-01-02 05:05:15 +0700

    Add PostTagController 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/multitenancy/controller/PostTagController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.multitenancy.controller;
+
+import com.hendisantika.multitenancy.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/2/24
+ * Time: 05:04
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+public class PostTagController {
+
+    private final PostService postService;
+}
