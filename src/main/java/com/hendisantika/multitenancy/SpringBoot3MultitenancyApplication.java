f3beeceeb3e8c428dbea08a6a2cffbfaed59b6e2
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:49:51 +0700

    Add modelMapper 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/multitenancy/SpringBoot3MultitenancyApplication.java
+++ src/main/java/com/hendisantika/multitenancy/SpringBoot3MultitenancyApplication.java
@@ -1,8 +1,10 @@
 package com.hendisantika.multitenancy;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Properties;
 
@@ -12,6 +14,11 @@ public class SpringBoot3MultitenancyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot3MultitenancyApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
