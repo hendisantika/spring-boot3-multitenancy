6f218b77deafaa0a0506ab4b5552cb79243b8aea
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:40:05 +0700

    Add Translator 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/multitenancy/util/Translator.java
@@ -0,0 +1,34 @@
+package com.hendisantika.multitenancy.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/1/24
+ * Time: 07:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class Translator {
+
+    private static ResourceBundleMessageSource messageSource;
+
+    @Autowired
+    Translator(ResourceBundleMessageSource messageSource) {
+        Translator.messageSource = messageSource;
+    }
+
+    public static String toLocale(String msg) {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(msg, null, locale);
+    }
+}
