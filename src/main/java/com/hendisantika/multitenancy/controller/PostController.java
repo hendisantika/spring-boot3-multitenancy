a35a1ba15dc166d41b50b6453dd296d9418d6bc5
hendisantika <hendisantika@yahoo.co.id>
2024-01-02 05:03:49 +0700

    Add getAllPosts in PostController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/multitenancy/controller/PostController.java
+++ src/main/java/com/hendisantika/multitenancy/controller/PostController.java
@@ -1,8 +1,16 @@
 package com.hendisantika.multitenancy.controller;
 
+import com.hendisantika.multitenancy.entity.Post;
 import com.hendisantika.multitenancy.service.PostService;
+import com.hendisantika.multitenancy.util.PageUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,4 +29,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @GetMapping("/v1/posts")
+    public ResponseEntity<Page<Post>> getAllPosts(
+            Pageable pageable, @RequestParam(required = false) String title) {
+
+        Page<Post> list = postService.getAllPosts(PageUtils.pageable(pageable), title);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
