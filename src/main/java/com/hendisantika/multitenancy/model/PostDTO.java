397ade7103faabbcf0cfc3e6364b8665cb400047
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:21:23 +0700

    Add PostDTO 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/multitenancy/model/PostDTO.java
@@ -0,0 +1,37 @@
+package com.hendisantika.multitenancy.model;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/1/24
+ * Time: 07:20
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDTO {
+
+    private long id;
+
+    @Column(name = "title", nullable = false)
+    @NotBlank(message = "Title is mandatory")
+    private String title;
+
+    @Column(name = "body")
+    @NotBlank(message = "Body is mandatory")
+    private String body;
+
+    private long authorId;
+}
