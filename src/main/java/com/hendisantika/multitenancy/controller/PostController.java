d68d8cef3a840c19c26a28d2f29a5dc4528b970f
hendisantika <hendisantika@yahoo.co.id>
2024-01-02 05:04:05 +0700

    Add getPostById in PostController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/multitenancy/controller/PostController.java
+++ src/main/java/com/hendisantika/multitenancy/controller/PostController.java
@@ -8,10 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,5 +33,11 @@ public class PostController {
 
         Page<Post> list = postService.getAllPosts(PageUtils.pageable(pageable), title);
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/v1/posts/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable("id") Long id) {
+        Post entity = postService.getById(id);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 }
