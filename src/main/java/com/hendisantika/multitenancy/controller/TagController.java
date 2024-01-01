af0d31db54d97a3d5d7cb4e6168106b1a1469320
hendisantika <hendisantika@yahoo.co.id>
2024-01-02 05:07:36 +0700

    Add getTagById in TagController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/multitenancy/controller/TagController.java
+++ src/main/java/com/hendisantika/multitenancy/controller/TagController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,5 +33,11 @@ public class TagController {
     public ResponseEntity<List<Tag>> getAllTags() {
         List<Tag> list = tagService.getAllTags();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/v1/tags/{id}")
+    public ResponseEntity<Tag> getTagById(@PathVariable("id") Long id) {
+        Tag entity = tagService.getById(id);
+        return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 }
