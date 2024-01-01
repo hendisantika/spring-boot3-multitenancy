0c2ec03c600603ab13453e78eafe13af24abbb9b
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:28:06 +0700

    Add deletePosts in PostRepository 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/multitenancy/repository/PostRepository.java
+++ src/main/java/com/hendisantika/multitenancy/repository/PostRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -28,4 +29,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query(value = "SELECT p FROM Post p WHERE lower(p.title) LIKE lower(concat('%', ?1,'%'))")
     List<Post> selectPosts(String title);
 
+    @Modifying
+    @Query("DELETE FROM Post p WHERE p.title = :title OR p.body = :body")
+    int deletePosts(@Param("title") String title, @Param("body") String body);
 }
