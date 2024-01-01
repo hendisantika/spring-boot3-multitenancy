bf17f8b6ecc41867a2688ad38a460939788923e3
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:50:16 +0700

    Add PageableConfig 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/multitenancy/config/PageableConfig.java
@@ -0,0 +1,31 @@
+package com.hendisantika.multitenancy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/1/24
+ * Time: 07:50
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class PageableConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setOneIndexedParameters(true);
+        resolver.setFallbackPageable(Pageable.unpaged());
+        resolvers.add(resolver);
+    }
+}
