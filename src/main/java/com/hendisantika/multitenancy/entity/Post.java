95dc12cfb1ceece33f3ca52cd074152c7f5d540a
hendisantika <hendisantika@yahoo.co.id>
2024-01-01 07:24:56 +0700

    Add Tag Entity 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/multitenancy/entity/Post.java
+++ src/main/java/com/hendisantika/multitenancy/entity/Post.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
---
+++ src/main/java/com/hendisantika/multitenancy/entity/Tag.java
@@ -0,0 +1,47 @@
+package com.hendisantika.multitenancy.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-multitenancy
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/1/24
+ * Time: 07:23
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "tags")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag extends Base implements Serializable {
+
+    private static final long serialVersionUID = 7156526087883281653L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name = "tag_id"),
+            inverseJoinColumns = @JoinColumn(name = "post_id"))
+    @JsonIgnoreProperties("tagList")
+    @JsonIgnore
+    private List<Post> postList;
+}
