5e692987ca042b11a778b3daa963874a32b54133
hendisantika <hendisantika@yahoo.co.id>
2024-01-02 05:05:54 +0700

    Add addTag in PostTagController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/multitenancy/controller/PostTagController.java
+++ src/main/java/com/hendisantika/multitenancy/controller/PostTagController.java
@@ -5,10 +5,7 @@ import com.hendisantika.multitenancy.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +30,11 @@ public class PostTagController {
     public ResponseEntity<List<Tag>> getAllTagsByPostId(@PathVariable(value = "id") Long id) {
         List<Tag> tagList = postService.getAllTagsByPostId(id);
         return new ResponseEntity<>(tagList, HttpStatus.OK);
+    }
+
+    @PostMapping("/v1/posts/{id}/tags")
+    public ResponseEntity<Tag> addTag(@PathVariable("id") Long id, @RequestBody Tag tagRequest) {
+        Tag updated = postService.addTag(id, tagRequest);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 }
